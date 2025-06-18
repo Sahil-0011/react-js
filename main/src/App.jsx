b16dff12cca496713f9f1dac686f17c66677ee7f
Sahil-011 <sahil9988776@gmail.com>
2025-06-18 23:36:39 +0530

    password generator

---
+++ main/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainPassword from './05passwordGenerator/mainPassword'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <MainPassword/>
+    
+      
+    </>
+  )
+}
+
+export default App
